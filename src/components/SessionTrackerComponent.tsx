675e8a5350dbbe30259c5806f781f6bccabfe189
Rodrigo Barraza <hello@rod.dev>
2026-07-20 18:52:33 -0700

    feat: enable replay and heatmap features in SessionTracker and update workspace dependencies

--- src/components/SessionTrackerComponent.tsx
+++ src/components/SessionTrackerComponent.tsx
@@ -4,5 +4,5 @@ import { SessionTrackerComponent as LibrarySessionTracker } from "@rodrigo-barra
 import { PROJECT_NAME } from "@/config";
 
 export default function SessionTrackerComponent() {
-  return <LibrarySessionTracker projectId={PROJECT_NAME} />;
+  return <LibrarySessionTracker projectId={PROJECT_NAME} replay heatmap />;
 }
